400ec56595cb990e41153c52be2ce5bfa37baf51
Stas <46508483+Stas9iMba@users.noreply.github.com>
2021-05-19 22:02:38 +0700

    slider section

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require("gulp");
 
 const scss = require("gulp-sass");
 const concat = require("gulp-concat");
@@ -19,7 +25,9 @@ function browsersync() {
 
 function styles() {
   return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(scss({
+      outputStyle: "compressed"
+    }))
     .pipe(concat("style.min.css"))
     .pipe(
       autoprefixer({
@@ -32,7 +40,10 @@ function styles() {
 }
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js", "app/js/main.js"])
+  return src(["node_modules/jquery/dist/jquery.js",
+      "node_modules/slick-carousel/slick/slick.js",
+      "app/js/main.js"
+    ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
@@ -43,11 +54,22 @@ function images() {
   return src("app/images/**/*.*")
     .pipe(
       imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.gifsicle({
+          interlaced: true
+        }),
+        imagemin.mozjpeg({
+          quality: 75,
+          progressive: true
+        }),
+        imagemin.optipng({
+          optimizationLevel: 5
+        }),
         imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          plugins: [{
+            removeViewBox: true
+          }, {
+            cleanupIDs: false
+          }],
         }),
       ])
     )
