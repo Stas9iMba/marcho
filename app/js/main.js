8d0a481376ef44696af08ebfc0f411e175f71362
Stas <46508483+Stas9iMba@users.noreply.github.com>
2021-06-10 01:10:55 +0700

    added shop content filter

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,8 @@ $(function () {
     fade: true,
   });
 
+	$('.select-style, .product-one__item-num').styler();
+
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
